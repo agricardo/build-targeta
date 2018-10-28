9644c51acfd25df4b9dbd4047e8a015ec6b1baaf
agricardo <raranda@techlaunch.io>
2018-10-27 22:52:09 -0400

    first commit

---
+++ build/precache-manifest.f6afbaf581e579b520ca3d80554beecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a103485420c8084dbbd9909f735e499d",
+    "url": "/static/media/logo.a1034854.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f9087050cbc27c56070",
+    "url": "/static/js/main.4f908705.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "4f9087050cbc27c56070",
+    "url": "/static/css/main.d8de82e3.chunk.css"
+  },
+  {
+    "revision": "65329c85f13326df42091cd7d48adae7",
+    "url": "/index.html"
+  }
+];
